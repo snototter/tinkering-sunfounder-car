96ec92b9ad9495446d6d518c7728254f66eefb21
snototter <h.possegger@gmail.com>
2018-06-14 22:55:44 +0200

    experimented with absolute values from gamepad stick

--- gamepad-ctrl/gamepad_ctrl.py
+++ gamepad-ctrl/gamepad_ctrl.py
@@ -27,7 +27,12 @@ class GamepadController:
                 'BTN_THUMB2' : self.req_bwd,
   #btn_thumb right
   # btn_top left
-                }
+                },
+            'Absolute' : {
+                'ABS_RZ' : (self.req_fwd, -1, 255), # values less than the middle (127-131) indicate that we should move forward (we'll check the sign)
+                #TODO self.req_driving_stick255 (instead of tuple) map 0-0.45 to fwd, 0.45-0.55 to stop, 0.55+ to backward
+                },
+            'Sync' : { 'SYN_REPORT' : self.req_ignore } # To avoid spamming the logs while testing the events triggered by our gamepad
             }
 # Add Absolute - map 0..127..255 to -1 0 1, pass to move_fwd()
         # We need to keep track of the servo states programmatically for now, to avoid killing the servos
@@ -53,16 +58,32 @@ class GamepadController:
             # ev_type == analog ? map value to true/false according to the mapped req_* callback
             # Otherwise, just pass the event state as is.
             type_map = self.event_mapping[event.ev_type]
-            if event.code in type_map:
-                type_map[event.code](event.state)
+            #if type(type_map) is tuple:
+            if event.ev_type == 'Absolute':
+                if event.code in type_map:
+                    # We need to map the stick values 0...max_axis to True/False - thus, our event/function mapping contains a tuple
+                    fhandle, sgn, max_axis = type_map[event.code]
+                    # Map the absolute value to boolean flag
+                    bvalue = self.map_absolute_value(event.state, sgn, max_axis)
+                    # Process request as usual:
+                    fhandle(bvalue)
+                else:
+                    print('  Unmapped CODE: {}, {}, {}'.format(event.ev_type, event.code, event.state))
             else:
-                print('  Unmapped CODE: {}, {}, {}'.format(event.ev_type, event.code, event.state))
+                if event.code in type_map:
+                    type_map[event.code](event.state)
+                else:
+                    print('  Unmapped CODE: {}, {}, {}'.format(event.ev_type, event.code, event.state))
         else:
             print('  Unmapped EV_TYPE: {}, {}, {}'.format(event.ev_type, event.code, event.state))
 
-    def map_analog_value(self, state, callback_handle):
+    def map_absolute_value(self, value, desired_sign, max_value):
         '''Maps the analog stick values to binary states required for the req_* method'''
-        pass
+        # TODO parametrize: our stick isn't perfectly centered and flips between 125--131 at the center position, thus, we require a minimum offset of ~10 %
+        if desired_sign < 0:
+            return value < 0.45 * max_value
+        else:
+            return value > 0.55 * max_value
 
     def req_drive(self, ctrl_callback, desired_state, event_value):
         """Invoke the given control callback if we're not in the desired state. Otherwise, stop moving. Leveraged by our req_fwd/bwd request handlers."""
@@ -93,6 +114,10 @@ class GamepadController:
         else:
             # TODO Raise exception
             pass
+
+    def req_ignore(self, value):
+        """Do nothing"""
+        pass
     
 
 def main():
