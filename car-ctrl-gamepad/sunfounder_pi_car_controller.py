d4e02c860a5f4c2fc5dcdf82f4f5e4c8cc7f9582
snototter-pi <h.possegger@gmail.com>
2018-06-17 19:32:12 +0000

    init sunfounder motor/steering module

--- car-ctrl-gamepad/sunfounder_pi_car_controller.py
+++ car-ctrl-gamepad/sunfounder_pi_car_controller.py
@@ -7,13 +7,21 @@ import sys
 sys.path.append('sunfounder-patched')
 import car_dir as sfsteering
 import motor as sfdriving
-import video_dir as sfpantilt
+#import video_dir as sfpantilt
+
+#video_dir.setup(busnum=busnum)
+#car_dir.setup(busnum=busnum)
+#motor.setup(busnum=busnum)     # Initialize the Raspberry Pi GPIO connected to the DC motor. 
+#video_dir.home_x_y()
+#car_dir.home()
 
 
 class GpioCarController(CarController):
     """To be used on the pi, accesses the pins directly."""
     def __init__(self):
-        pass
+        self.busnum = 1
+        sfdriving.setup(busnum=self.busnum)
+        sfsteering.setup(busnum=self.busnum)
 
     def drive_forward(self):
         sfdriving.forward()
