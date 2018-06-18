6b047f91899f656cf5b91de96fe241941dbb244e
snototter-pi <h.possegger@gmail.com>
2018-06-18 20:09:09 +0000

    working pi/laptop communication

--- bt-sandbox/bt_server.py
+++ bt-sandbox/bt_server.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# TODO sudo hciconfig hci0 piscan
+# otherwise, client won't find the pi!
 """
 A simple Python script to receive messages from a client over
 Bluetooth using PyBluez (with Python 2).
@@ -6,7 +8,8 @@ Bluetooth using PyBluez (with Python 2).
 
 import bluetooth
 
-hostMACAddress = '4C:34:88:E1:EB:54' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+#hostMACAddress = '4C:34:88:E1:EB:54' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+hostMACAddress = 'B8:27:EB:9B:1B:EA'
 port = 3
 backlog = 1
 size = 1024
@@ -14,7 +17,9 @@ s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.bind((hostMACAddress, port))
 s.listen(backlog)
 try:
+    print('waiting')
     client, clientInfo = s.accept()
+    print('connected')
     while 1:
         data = client.recv(size)
         if data:
